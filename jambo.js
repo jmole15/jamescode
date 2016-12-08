070751caa30c6805ea514a1f8b2aa330eee8ef50
jmole15 <jmole15@mail.com>
2016-12-08 20:49:47 +0000

    Add files via upload

---
+++ jambo.js
@@ -0,0 +1,5 @@
+var num1 = prompt("enter a number");
+if (num1 > 10) {alert(num1 + " is bigger than 10")};
+if (num1 < 10) {alert(num1 + " is smaller than 10")};
+if (num1 == 10) {alert("this number is 10")};
+// to tell you that 10 is = to 10
